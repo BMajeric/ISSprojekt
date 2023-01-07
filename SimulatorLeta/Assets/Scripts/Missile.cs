f8dae7e45ea5a97e2d8af41ab35ffc82ab96b461
BMajeric <borna.majeric@gmail.com>
2023-01-07 19:39:56 +0100

    popravljeno kretanje rakete s avionom

--- SimulatorLeta/Assets/Scripts/Missile.cs
+++ SimulatorLeta/Assets/Scripts/Missile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,24 +6,33 @@ using UnityEngine;
 public class Missile : MonoBehaviour
 {
     Rigidbody _rb;
+    public GameObject _go;
     
     [Header("Missile attributes")]
     [Tooltip("The speed at which the missile is fired.")]
-    private float _speed = 10f;
+    public float _speed = 10f;
     [Tooltip("The indicator if the missle is fired or if its still loaded.")]
     private bool isFired;
+    [Tooltip("The initial offset in position between the plane and the missile.")]
+    private Vector3 positionOffset;
+    [Tooltip("The initial offset in rotation between the plane and the missile.")]
+    private Quaternion rotationOffset;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
         isFired = false;
+        positionOffset = _rb.transform.localPosition;
+        rotationOffset = _rb.transform.localRotation;
     }
 
     void FireMissile()
     {
-        Vector3 localForward = _rb.transform.worldToLocalMatrix.MultiplyVector(transform.forward);
-        _rb.AddForce(-localForward * _speed, ForceMode.Impulse);
+        // Vector3 localForward = _rb.transform.worldToLocalMatrix.MultiplyVector(transform.forward);
+        Vector3 localForward = _rb.transform.right;
+        _rb.AddForce(localForward * _speed, ForceMode.Impulse);
     }
 
     // Update is called once per frame
@@ -32,6 +42,11 @@ public class Missile : MonoBehaviour
         {
             FireMissile();
             isFired = true;
+        }
+        else if (!isFired)
+        {
+            _rb.transform.localPosition = positionOffset;
+            _rb.transform.localRotation = rotationOffset;
         }
     }
 }
