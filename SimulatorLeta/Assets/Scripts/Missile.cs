24fbffd6c97f13f71bddec94bffbf40d53d85163
BMajeric <borna.majeric@gmail.com>
2023-01-09 02:16:02 +0100

    New missile, added explosion and missile trail and the missile now works properly

--- SimulatorLeta/Assets/Scripts/Missile.cs
+++ SimulatorLeta/Assets/Scripts/Missile.cs
@@ -7,16 +7,27 @@ public class Missile : MonoBehaviour
 {
     Rigidbody _rb;
     [SerializeField] private Transform RaycastStart;
-    
+
     [Header("Missile attributes")]
+
     [Tooltip("The speed at which the missile is fired.")]
-    public float _speed = 10f;
+    [SerializeField] private float _speed = 20f;
+
+    [Tooltip("Smoke trail model.")]
+    [SerializeField] private GameObject _smokeTrail;
+
+    [Tooltip("Explosion model.")]
+    [SerializeField] private GameObject _explosion;
+
     [Tooltip("The indicator if the missle is fired or if its still loaded.")]
     private bool isFired;
+
     [Tooltip("The initial offset in position between the plane and the missile.")]
     private Vector3 positionOffset;
+
     [Tooltip("The initial offset in rotation between the plane and the missile.")]
     private Quaternion rotationOffset;
+
     [Tooltip("The result of a Raycast hit.")]
     private RaycastHit hit;
     private Vector3 targetPosition;
@@ -30,17 +41,35 @@ public class Missile : MonoBehaviour
         rotationOffset = _rb.transform.localRotation;
     }
 
-    void FireMissile()
+    void Start()
     {
-        Vector3 localForward = _rb.transform.right;
+        _smokeTrail.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Destructable"))
+        {
+            Instantiate(_explosion, collision.transform.position, Quaternion.identity);
+            Destroy(collision.collider.gameObject);
+        }
+
+        Destroy(gameObject);
+    }
+
+    private void FireMissile()
+    {
+        _smokeTrail.SetActive(true);
+        Vector3 localForward = transform.forward;
         _rb.AddForce(localForward * _speed, ForceMode.Impulse);
     }
 
-    void RotateMissileToTraget()
+    private void RotateMissileToTraget()
     {
         Vector3 missileDirection = targetPosition - _rb.transform.position;
-        Quaternion rotation = Quaternion.LookRotation(missileDirection);
+        Quaternion rotation = Quaternion.LookRotation(missileDirection, missileDirection);
         _rb.MoveRotation(rotation);
+        
     }
 
     // Update is called once per frame
@@ -53,7 +82,10 @@ public class Missile : MonoBehaviour
         {
             Debug.DrawRay(RaycastStart.position, -RaycastStart.TransformDirection(Vector3.forward) * hit.distance, Color.red);
             targetPosition = hit.point;
-            RotateMissileToTraget();
+            if (!isFired)
+            {
+                RotateMissileToTraget();
+            }
 
             if (Input.GetButtonDown("Fire1") && !isFired)
             {
@@ -83,6 +115,5 @@ public class Missile : MonoBehaviour
             }
         }
 
-        
     }
 }
