82fc0d73dbb2133b81f0eb71e71adb97065078df
fransaganic <fran.saganic@fer.hr>
2023-01-19 12:06:19 +0100

    Menu

--- SimulatorLeta/Assets/Scripts/EndOfTheGame.cs
+++ SimulatorLeta/Assets/Scripts/EndOfTheGame.cs
@@ -6,7 +6,9 @@ using UnityEngine.SceneManagement;
 public class EndOfTheGame : MonoBehaviour
 {
     public void GoBack (){
-
+        
+        Time.timeScale = 1f;
+        PauseMenu.isPaused = false;
         SceneManager.LoadScene(0);
     }
 
